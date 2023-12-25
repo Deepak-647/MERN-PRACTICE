4fa5eaae0fbfef0fabf5f9b70a61eff04f6caf27
Deepak <dbehera647@gmail.com>
2023-12-25 19:46:49 +0530

    Storing Registered User Data in MongoDB Atlas

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,22 +1,25 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
-    res
-    .status(200)
-    .send("Welcome to the page using Controller");
-  } catch(error){
+    res.status(200).send("Welcome to the page using Controller");
+  } catch (error) {
     console.log(error);
   }
 };
 const register = async (req, res) => {
   try {
-    console.log(req.body)
+    const { username, email, phone, password } = req.body;
+    const userExist = await User.findOne({ email });
 
-    res
-    .status(200)
-    .json(req.body);
-  } catch(error){
+    if (userExist) {
+      return res.status(400).json({ msg: "Email already exist" });
+    }
+    const userCreated = await User.create({ username, email, phone, password });
+    res.status(200).json({ msg: userCreated });
+  } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = {home,register};
+module.exports = { home, register };
