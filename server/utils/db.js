7f9f3fd750b46fd9c880b6930bc90922b3873887
Deepak <dbehera647@gmail.com>
2023-12-25 19:29:49 +0530

    Creating User Schema & Model for User Registration

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,15 +1,15 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-const URI = process.env.MONGODB_URI ;
+const URI = process.env.MONGODB_URI;
 
-const connectDb = async()=>{
-try{
-await mongoose.connect(URI)
-console.log("connection successful to database")
-}catch(error){
-    console.error("database connection failed")
+const connectDb = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("connection successful to database");
+  } catch (error) {
+    console.error("database connection failed");
     process.exit(0);
-}
-}
+  }
+};
 
 module.exports = connectDb;
