5e9eaaf895b61fa3aa6f70ecb8195d944882b688
Deepak <dbehera647@gmail.com>
2023-12-25 18:54:10 +0530

    Connecting Backend with MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app= express();
 const router = require("./router/auth-route")
+const connectDb = require("./utils/db")
 
 app.use(express.json()) //This is used to allow express to deal with JSON
 
@@ -11,6 +12,9 @@ res.status(200).send("Welcome to My Home")
 })
 
 const PORT = 5000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on Port ${PORT}`)
+
+connectDb().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on Port ${PORT}`)
+    })
 })
