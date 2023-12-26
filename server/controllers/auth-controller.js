717580ac1f0c5a38d7d9a7219a4de71a543835d0
Deepak <dbehera647@gmail.com>
2023-12-26 08:50:29 +0530

    Securing User Authentiction using JWT Token

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,7 +1,5 @@
 const User = require("../models/user-model");
 
-
-
 const home = async (req, res) => {
   try {
     res.status(200).send("Welcome to the page using Controller");
@@ -17,11 +15,13 @@ const register = async (req, res) => {
     if (userExist) {
       return res.status(400).json({ msg: "Email already exist" });
     }
-    
-
 
     const userCreated = await User.create({ username, email, phone, password });
-    res.status(200).json({ msg: userCreated });
+    res.status(200).json({
+      msg: "Registration Successfull",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
   } catch (error) {
     console.log(error);
   }
