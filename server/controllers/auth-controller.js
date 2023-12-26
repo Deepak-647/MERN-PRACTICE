f705fdd10ce4b1f5c01e16c06592e3e8c217ff3a
Deepak <dbehera647@gmail.com>
2023-12-26 10:33:33 +0530

    Building a Custom Compare Password Check Function

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -37,7 +37,7 @@ const login = async (req,res) => {
       return res.status(400).json("Invalid Credentials")
     }
 
-    const user =await bcrypt.compare(password,userExist.password)
+    const user =await userExist.comparePassword(password)
     if(user){
       res.status(200).json({
         msg: "Login Successfull",
