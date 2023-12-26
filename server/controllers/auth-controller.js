95c2b57609b84c9746846802d8d50a7c8ee29939
Deepak <dbehera647@gmail.com>
2023-12-26 10:16:49 +0530

    Building a Complete User Login Route with Functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
   try {
@@ -27,4 +28,30 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { home, register };
+const login = async (req,res) => {
+  try {
+    const {email , password} =req.body;
+    const userExist = await User.findOne({email});
+
+    if(!userExist){
+      return res.status(400).json("Invalid Credentials")
+    }
+
+    const user =await bcrypt.compare(password,userExist.password)
+    if(user){
+      res.status(200).json({
+        msg: "Login Successfull",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+      });
+    }else{
+      res.status(401).json({Message : "Invalid email or password"})
+    }
+  } catch (error) {
+    res.status(500).json("Internal Server Error")
+    console.log(error)
+  }
+
+}
+
+module.exports = { home, register ,login };
