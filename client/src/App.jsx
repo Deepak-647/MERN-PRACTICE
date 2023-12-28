26bedf0bc69f42421154408428e41e00cc6700f1
Deepak <dbehera647@gmail.com>
2023-12-28 12:15:06 +0530

    React Page Navigation with React Router DOM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
-  return <h1>Hello to the React Page</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
