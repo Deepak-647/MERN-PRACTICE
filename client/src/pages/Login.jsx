8ebee92fdd4f7df810b0438be013c50d99cb8ceb
Deepak <dbehera647@gmail.com>
2023-12-28 18:21:45 +0530

    Creating Log In Page & stored Form data in React State Variable

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,80 @@
-
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]:value,
+    })
+  };
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    
+  }
   return (
-    <div>Login</div>
+    <div>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/login.png"
+                  alt="registration-image"
+                  width="500"
+                  height="500"
+                />
+              </div>
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">Log In</h1>
+                <br />
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Log In
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
   )
 }
 
