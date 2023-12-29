341fb57314b32837c7158542f996ffacf6bf0ce2
Deepak <dbehera647@gmail.com>
2023-12-29 09:44:18 +0530

    Connecting React with Backend and storing register data in database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const app= express();
 const authRoute = require("./router/auth-route")
 const contactRoute = require("./router/contact-route")
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
+
+//handling cors
+const corsOptions = {
+    origin:"http://localhost:5173",
+    methods: "GET , POST , PUT , DELETE , PATCH , HEAD" ,
+    credentials : true,
+}
+app.use(cors(corsOptions))
+
 
 app.use(express.json()) //This is used to allow express to deal with JSON
 
