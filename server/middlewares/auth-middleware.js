b2add7856f5168a2da87f779f837cd3aa35fe27e
Deepak <dbehera647@gmail.com>
2024-01-03 12:14:06 +0530

    Securing our Admin Route with JWT Verification and Middleware Integration

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -3,6 +3,9 @@ const User = require("../models/user-model");
 
 const authMiddleware = async (req, res, next) => {
   const token = req.header("Authorization");
+  if(!token){
+    return res.status(401).json({message : "Unauthorized HTTP , Token not provided"})
+  }
   const jwtToken = token.replace("Bearer", "").trim();
 
   try {
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {getAllUsers,getAllContacts} = require("../controllers/admin-controller")
- 
+const authMiddleware = require("../middlewares/auth-middleware")
 
-router.route('/users').get(getAllUsers);
-router.route('/contacts').get(getAllContacts);
+router.route('/users').get(authMiddleware,getAllUsers);
+router.route('/contacts').get(authMiddleware,getAllContacts);
 
 module.exports =router;
