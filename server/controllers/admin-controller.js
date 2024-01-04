bccc4a3bf4b32ecec109df3615a0150810652d68
Deepak <dbehera647@gmail.com>
2024-01-04 19:20:38 +0530

    Updating User Data | Backend Route & Logics

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,45 +1,61 @@
 const User = require("../models/user-model");
 const Contact = require("../models/contact-model");
 
-const getAllUsers = async (req,res) => {
- try {
-    const users = await User.find({},{password : 0})
-    if(!users || users.length === 0){
-        return res.status(404).json({message : "No Users Found"})
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, { password: 0 });
+    if (!users || users.length === 0) {
+      return res.status(404).json({ message: "No Users Found" });
     }
     return res.status(200).json(users);
- } catch (error) {
-    next(error)
- }
-}
-const getAllContacts = async (req,res) => {
- try {
-    const contacts = await Contact.find({},{password : 0})
-    if(!contacts || contacts.length === 0){
-        return res.status(404).json({message : "No Contacts Found"})
+  } catch (error) {
+    next(error);
+  }
+};
+const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find({}, { password: 0 });
+    if (!contacts || contacts.length === 0) {
+      return res.status(404).json({ message: "No Contacts Found" });
     }
     return res.status(200).json(contacts);
- } catch (error) {
-    next(error)
- }
-}
-const deleteUserById = async (req,res) =>{
-try {
-   const id = req.params.id;
-   await User.deleteOne({_id :id});
-   return res.status(200).json({message:"User Deleted Successfully!"})
-} catch (error) {
-   next(error)
-}
-}
+  } catch (error) {
+    next(error);
+  }
+};
+const deleteUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await User.deleteOne({ _id: id });
+    return res.status(200).json({ message: "User Deleted Successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const getUsersById = async (req,res) => {
-   try {
-      const id = req.params.id;
-      const data = await User.findOne({_id:id},{password :0});
-      return res.status(200).json(data)
-   } catch (error) {
-      nect(error);
-   }
-}
-module.exports = {getAllUsers,getAllContacts,deleteUserById,getUsersById};
+const updateUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedUserData = req.body;
+
+    const updateUser = await User.updateOne(
+      { _id: id },
+      {
+        $set: updatedUserData,
+      }
+    );
+    return res.status(200).json(updateUser);
+  } catch (error) {
+    next(error);
+  }  
+};
+const getUsersById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await User.findOne({ _id: id }, { password: 0 });
+    return res.status(200).json(data);
+  } catch (error) {
+    nect(error);
+  }
+};
+module.exports = { getAllUsers, getAllContacts, deleteUserById, getUsersById ,updateUserById};
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {getAllUsers,getAllContacts, deleteUserById, getUsersById} = require("../controllers/admin-controller")
+const {getAllUsers,getAllContacts, deleteUserById, getUsersById, updateUserById} = require("../controllers/admin-controller")
 const authMiddleware = require("../middlewares/auth-middleware");
 const adminMiddleware = require("../middlewares/admin-middleware");
 
 router.route('/users').get(authMiddleware,adminMiddleware,getAllUsers);
 router.route('/users/:id').get(authMiddleware,adminMiddleware,getUsersById);
+router.route('/users/update/:id').patch(authMiddleware,adminMiddleware,updateUserById);
 router.route('/users/delete/:id').delete(authMiddleware,adminMiddleware,deleteUserById)
 router.route('/contacts').get(authMiddleware,adminMiddleware,getAllContacts);
 
