0701b80c85db7047cdbe237a083315ae7cae7f94
Deepak <dbehera647@gmail.com>
2024-01-05 12:18:11 +0530

    Displaying all Contact Data in our Admin Contact Page

--- client/src/pages/AdminContacts.jsx
+++ client/src/pages/AdminContacts.jsx
@@ -1,3 +1,39 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../store/auth";
+ 
+
 export const AdminContacts = () => {
-    return <h1>AdminContacts</h1>
-}
+  const { authorizationToken } = useAuth();
+  const [contacts, setContacts] = useState([]);
+  const getAllContactsData = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/admin/contacts", {
+        headers: {
+          Authorization: authorizationToken,
+        },
+      });
+      const data = await response.json();
+      // console.log(data)
+      setContacts(data);
+    } catch (error) {
+      console.log("Error in Fetching All Users Data");
+    }
+  };
+  useEffect(() => {
+    getAllContactsData();
+  }, []);
+  return (
+    <>
+      <h1>AdminContacts</h1>
+      <div>
+        {contacts.map((contact) => (
+          <>
+            <p>{contact.username}</p>
+            <p>{contact.email}</p>
+            <p>{contact.message}</p>
+          </>
+        ))}
+      </div>
+    </>
+  );
+};
